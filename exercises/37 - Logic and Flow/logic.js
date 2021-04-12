56577ff4b2d2ca9a275225f13d68791c24774e76
nateliason <nateliason@gmail.com>
2021-04-12 11:21:15 -0500

    up through video 41

---
+++ exercises/37 - Logic and Flow/logic.js
@@ -0,0 +1,16 @@
+// ternary functions
+
+const count = 2;
+// let word;
+
+// if (count === 1) {
+//   word = 'item';
+// } else {
+//   word = 'items';
+// }
+
+// 1. Condition
+// 2. What to do if true
+// 3. What to do if false
+
+const word = count === 1 ? 'item' : 'items';
